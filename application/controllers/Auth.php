4e047604b329054f1c3fde2774567564800f906f
Aris Firmansyah <arisfirmansyh29@gmail.com>
2023-02-09 20:38:00 +0700

    first commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+
+	public function login()
+	{
+		$this->load->view('login');
+	}
+
+	public function process()
+	{
+		$post = $this->input->post(null, TRUE);
+		if(isset($post['login'])) {
+			$this->load->model('user_m');
+			$query = $this->user_m->login($post);
+			if($query->num_rows() > 0) {
+				$row = $query->row();
+				$params = array(
+					'userid' => $row->user_id,
+					'level' => $row->level
+				);
+				$this->session->set_userdata($params);
+				echo "<script>
+					alert('Selamat, Login Berhasil');
+					window.location='".site_url('dashboard')."';
+				</script>";
+			} else {
+				echo "<script>
+					alert('Login Gagal, Username atau Password Salah');
+					window.location='".site_url('auth/login')."';
+				</script>";
+			}
+		}
+	}
+}
