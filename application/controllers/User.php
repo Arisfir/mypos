8c6702be11671e6423fbf85218951176af2f9d87
Aris Firmansyah <arisfirmansyh29@gmail.com>
2023-02-11 13:48:17 +0700

    form-validation

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -33,6 +33,34 @@ class User extends CI_Controller
 
     public function add()
     {
-        $this->template->load('template', 'user/user_form_add');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('fullname', 'Nama', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[user.username]');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+        $this->form_validation->set_rules('passconf', 'Konfirmasi Password', 'required|matches[password]',
+            array('matches' => '%s tidak sesuai dengan password')
+        );
+        $this->form_validation->set_rules('address', 'Alamat', 'required');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+
+        $this->form_validation->set_message('required', '%s masih kosong, silahkan isi');
+        $this->form_validation->set_message('min_length', '{field} minimal 5 karakter');
+        $this->form_validation->set_message('is_unique', '{field} ini sudah dipakai, silahkan ganti');
+
+
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->template->load('template', 'user/user_form_add');
+        } else {
+            // $post = $this->input->post(null, TRUE);
+            // $this->user_m->add($post);
+            // if ($this->db->affected_rows() > 0) {
+            //     echo "<script>alert('Data berhasil disimpan');</script>";
+            // }
+            // echo "<script>window.location='" . site_url('user') . "';</script>";
+            echo "proses simpan data user baru";
+        }
+
     }
 }
--- application/views/user/user_data.php
+++ application/views/user/user_data.php
@@ -13,35 +13,12 @@
             </div>
 
         </div>
-    </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.container-fluid -->
 </section>
 
 <!-- Main content -->
 <section class="content">
-
-    <!-- Default box -->
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Title</h3>
-
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
-        </div>
-        <div class="card-body">
-            Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-            Footer
-        </div>
-        <!-- /.card-footer-->
-    </div>
     <!-- /.card -->
     <div class="card">
         <div class="card-header">
@@ -54,41 +31,45 @@
                 </a>
             </div>
         </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-                <div class="box-body table-responsive">
-                    <table class="table table-bordered table-striped">
-                        <thead>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <div class="box-body table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Username</th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Level</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1;
+                        foreach ($row->result() as $key => $data) { ?>
                             <tr>
-                                <th>No</th>
-                                <th>Username</th>
-                                <th>Name</th>
-                                <th>Address</th>
-                                <th>Level</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php $no = 1;
-                            foreach ($row->result() as $key => $data) { ?>
-                                <tr>
-                                    <td><?= $no++ ?></td>
-                                    <td><?= $data->username ?></td>
-                                    <td><?= $data->name ?></td>
-                                    <td><?= $data->address ?></td>
-                                    <td><?= $data->level == 1 ? "Admin" : "Kasir" ?></td>
-                                    <td>
-                                        <a href="<?= site_url('user/edit/' . $data->user_id) ?>" class="btn btn-primary btn-xs">
+                                <td><?= $no++ ?></td>
+                                <td><?= $data->username ?></td>
+                                <td><?= $data->name ?></td>
+                                <td><?= $data->address ?></td>
+                                <td><?= $data->level == 1 ? "Admin" : "Kasir" ?></td>
+                                <td>
+                                    <a href="<?= site_url('user/edit/' . $data->user_id) ?>" class="btn btn-primary btn-xs">
                                         <i class="fa fa-pen"></i> Update
-                                        </a>
-                                        <a href="<?= site_url('user/del/' . $data->user_id) ?>" onclick="return confirm('Apakah Anda Yakin?')" class="btn btn-danger btn-xs">
-                                            <i class="fa fa-trash"></i> Delete
-                                        </a>
-                                    </td>
-                                </tr>
-                            <?php } ?>
-                    </table>
-                </div>
+                                    </a>
+                                    <a href="<?= site_url('user/del/' . $data->user_id) ?>" onclick="return confirm('Apakah Anda Yakin?')" class="btn btn-danger btn-xs">
+                                        <i class="fa fa-trash"></i> Delete
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                </table>
             </div>
-
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            Footer
+        </div>
+    </div>
 </section>
